9ce908741fead48babebb6f9bedbc9591df5f091
unknown <1214620323@qq.com>
2020-06-14 16:09:57 +0800

    session 与 cookies完结

--- src/main/java/com/coedeshuai/community/community/controller/AuthorizeController.java
+++ src/main/java/com/coedeshuai/community/community/controller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author ：codeshuai
  * @date ：Created in 2020/6/14 10:34
@@ -28,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
@@ -38,7 +42,14 @@ public class AuthorizeController {
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+//            登录成功，写入cookie和session
+//            将user对象放入session中
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+//            登录失败,请重新登陆
+            return "redirect:/";
+        }
     }
 }
